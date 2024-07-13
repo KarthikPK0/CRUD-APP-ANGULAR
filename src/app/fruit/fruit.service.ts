ee20b7c9890170bacbf901133446c59fc35903d3
karthik-1825 <kxik4studies@gmail.com>
2024-07-13 15:50:41 +0530

    updated

--- src/app/fruit/fruit.service.ts
+++ src/app/fruit/fruit.service.ts
@@ -10,24 +10,24 @@ export class FruitService {
   constructor(private httpClient: HttpClient) { }
 
   getAll() {
-    return this.httpClient.get<Fruit[]>('http://localhost:3000/fruits');
+    return this.httpClient.get<Fruit[]>('https://crudapp-angular-server.onrender.com/fruits');
   }
 
   create(data: Fruit) {
  
-    return this.httpClient.post('http://localhost:3000/fruits', data);
+    return this.httpClient.post('https://crudapp-angular-server.onrender.com/fruits', data);
   }
 
   edit(id: number ){
-    return this.httpClient.get<Fruit>( `http://localhost:3000/fruits/${id}`);
+    return this.httpClient.get<Fruit>( `https://crudapp-angular-server.onrender.com/fruits/${id}`);
   }
 
   update(data: Fruit) {
-    return this.httpClient.put<Fruit>(`http://localhost:3000/fruits/${data.id}`, data);
+    return this.httpClient.put<Fruit>(`https://crudapp-angular-server.onrender.com/fruits/${data.id}`, data);
   }
 
   delete(id: number ){
-    return this.httpClient.delete<Fruit>( `http://localhost:3000/fruits/${id}`);
+    return this.httpClient.delete<Fruit>( `https://crudapp-angular-server.onrender.com/fruits/${id}`);
   }
 
 
